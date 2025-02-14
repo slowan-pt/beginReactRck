ba4a6f56559ddfe95b3fd0e4f0f8eb7a9bf74db0
Sloan Nascimento <sloan.nascimento@gmail.com>
2025-02-13 21:39:20 -0300

    feat: criação parcial dos módulos 'Avatar' e 'Avatar.module.css'. Incompleto

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 import {PencilLine} from '@phosphor-icons/react';
 
@@ -11,7 +12,7 @@ export function Sidebar() {
             />
         
             <div className={styles.profile}>
-                <img className={styles.avatar} src='/src/assets/avatar.jpg' ></img>
+                <Avatar src='/src/assets/avatar.jpg'/>
                 <strong>Usuário: Sloan Nascimento</strong>
                 <span>Cargo: Web Developer</span>
             </div>
