5aedaec6aa7680bb5212db582cc3d4a2c3b3499b
Sloan Nascimento <sloan.nascimento@gmail.com>
2025-02-27 04:19:08 -0300

    feat: 'Post.jsx' ALT datas e conteúdo

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale/pt-BR';
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
@@ -10,30 +12,44 @@ content: String
 
 */
 
-export function Post (props) {
-    console.log(props);
+export function Post ({author, publishedAt, content}) {
+
+    const publishedAtDateFormatInFull = format(publishedAt, "d 'de' LLLL 'às' HH':'mm'h'", {locale: ptBR})
+    const publishedAtDateFormatRelativeNow = formatDistanceToNow(publishedAt, {locale:ptBR, addSuffix: true})
+/* 
+Uma das fomas de manipular datas é com o INTL, mas dá pra usar o "npm i date-fns"
+const publishedAtDateFormatInFull = new Intl.DateTimeFormat 
+    ("pt-BR", {
+        day: '2-digit',
+        month: 'long',
+        hour: '2-digit',
+        minute: '2-digit'
+}).format(publishedAt);
+*/
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar hasBorder={true } src="/src/assets/avatar3.png" />
+                    <Avatar hasBorder={true} src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Sloan Nascimento </strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
-                <time title="11 de FEV às 04:51" dateTime="2025-05-23 04:49:20">Publicado há 1h</time>
+                <time title={publishedAtDateFormatInFull} dateTime={publishedAt.toISOString()}>
+                   {publishedAtDateFormatRelativeNow}
+                </time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala Galera </p>
-                <p>Acabei de subir mais um projeto </p>
-                <p> <a href=""> jane.design/doctorcare </a></p>
-                <p> 
-                    <a href="">#novoprojeto </a> {/*comentário: para dar espaço, ou dá um espaço no <a> ou {' '} */}
-                    <a href="">#nlw </a>
-                    <a href="">#rocketseat</a>
-                </p>
+                {content.map(line => {
+                    if (line.type === 'paragraph'){
+                        return <p>{line.content}</p>
+                    } else if (line.type === 'link'){
+                        return <p><a href='#'>{line.content}</a></p>
+                    }
+                })}
+               
             </div>
         <form className={styles.commentForm}>
             <strong>Deixe seu feedback</strong>  
